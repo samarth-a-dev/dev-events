5a3dca305df524f723e3fe192f9d43f4d3dc9eb9
Samarth Agrawal <samarthagrawal2328@gmail.com>
2026-09-27 02:32:30 +0530

    load real events final

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
 
 const Page = async () => {
     'use cache';
